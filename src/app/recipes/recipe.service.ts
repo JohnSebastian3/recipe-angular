fe60207729b12f6319bca1e683522821cf252f2a
Sebastian Guerrero <sebastianbiochem@gmail.com>
2023-07-05 14:45:04 -0400

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Seasoned Pork',
+      'A super tasy seasoned pork chop!',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Black Pepper', 1)]
+    ),
+    new Recipe(
+      'Shrimp Salad',
+      'A healthy shrimp salad perfect for any occasion.',
+      'https://www.publicdomainpictures.net/pictures/120000/nahled/shrimp-salad-1434452515ZNJ.jpg',
+      [new Ingredient('Lettuce', 2), new Ingredient('Shrimp (Jumbo)', 6)]
+    ),
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes() {
+    // send array copy
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
